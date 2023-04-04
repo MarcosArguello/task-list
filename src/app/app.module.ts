1df1f75f3766009e834a17adba4f9251e2205819
MarcosArguello <arguellomarcos@gmail.com>
2023-04-04 20:35:32 -0300

    Primer componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtomComponent } from './components/buttom/buttom.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtomComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
